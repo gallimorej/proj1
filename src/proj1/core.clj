f683000d14783c5ce925eb8da3df2470662c1eac
gallimorej <jeff.gallimore@excella.com>
2018-11-03 13:02:43 -0400

    Commit initial files to repo

---
+++ src/proj1/core.clj
@@ -0,0 +1,7 @@
+(ns proj1.core)
+
+(defn foo
+  "I don't do a whole lot. Yet."
+  [x]
+  (println x "Hello, World!"))
+
---
+++ test/proj1/core_test.clj
@@ -0,0 +1,7 @@
+(ns proj1.core-test
+  (:require [clojure.test :refer :all]
+            [proj1.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
