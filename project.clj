28b6520fa0e49699c14149de30037dc79323afc2
Gene Kim <genek@realgenekim.me>
2018-11-16 14:00:20 -0800

    added slack, working in REPL

--- project.clj
+++ project.clj
@@ -3,5 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
-  :main proj1.core/foo)
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [clj-http "3.9.1"]
+                 [org.clojure/data.json "0.2.6"]]
+  :main proj1.core)
---
+++ src/proj1/slack.clj
@@ -0,0 +1,20 @@
+(ns proj1.slack
+  (:require [clj-http.client :as client]
+            [clojure.data.json :as json]))
+
+
+; this is the #gallimore-stuff channel
+
+(def WEBHOOK-URL "https://hooks.slack.com/services/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")
+
+(defn send-to-slack
+  "Sends a simple message to slack using an 'incoming webhook'.
+   url will be of form: https://myapp.slack.com/services/hooks/incoming-webhook?token=mytoken .
+   (Exact url you should use will appear on the slack integration page)
+   text will be any valid message.
+   This implementation could be expanded if you wanted to specify channel, username, etc.
+   For more information see:
+   https://my.slack.com/services/new/incoming-webhook . (You'll need a slack account
+   to see that)"
+  [url text]
+  (client/post url {:form-params {:payload (json/write-str {:text text})}}))
