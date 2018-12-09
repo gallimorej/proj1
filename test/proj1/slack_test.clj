a0ad1d33c4261867a2441580a6bd0d2cf5ea38c9
gallimorej <jeff.gallimore@excella.com>
2018-12-09 14:32:08 -0500

    Add tests for Slack namespace

---
+++ test/proj1/slack_test.clj
@@ -0,0 +1,8 @@
+(ns proj1.slack-test
+  (:require [clojure.test :refer :all]
+            [proj1.slack :refer :all]))
+
+(deftest a-slack-test
+  (testing "Dummy test for now"
+    (is (= 0 0))
+    (is (not (= 0 1)))))
