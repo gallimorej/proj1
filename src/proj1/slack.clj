1c02053dbdef26391c37e3a58725dd7215730dbc
Gene Kim <genek@realgenekim.me>
2018-12-14 14:20:26 -0800

    after pairing session w/Jeff

--- src/proj1/slack.clj
+++ src/proj1/slack.clj
@@ -1,7 +1,7 @@
 (ns proj1.slack
   (:require [clj-http.client :as client]
             [clojure.data.json :as json]
-            [java-time]))
+            [java-time :as time]))
 
 ; this is the #clojure-hacking channel
 
@@ -21,33 +21,43 @@
   ([url text]
    (client/post url {:form-params {:payload (json/write-str {:text text})}})))
 
-(defn read-messages
+(defn read-messages!
   "Reads in the messages from a JSON file"
   []
   (json/read-str (slurp "data/messages.json") :key-fn keyword))
 
-(defn get-messages
-  "Gets the messages to be scheduled"
-  []
-  ;TODO: process the keywords to convert strings to other data types
-  ;(map (fn [x] (update x :scheduled-date-time #(local-date-time "yyyy/MM/dd HH:mm:ss" %)) (read-messages)))
-  (read-messages))
-
-(defn convert-date-time-str
-  "Converts a string to a date-time"
-  [the-date-time-str]
-  (local-date-time "yyyy-MM-dd HH:mm:ss" the-date-time-str))
-
-(defn fix-message-val-types!
+(defn fix-message-val-types
   "Fixes the datatypes for the values in the message, turning them from strings to other things
    side effect is that the message passed in changes"
   [m]
   (update m :scheduled-date-time convert-date-time-str))
 
+(defn get-messages!
+  "Gets the messages to be scheduled"
+  []
+  ;TODO: process the keywords to convert strings to other data types
+  ; same as below
+  ; (->> (read-messages!)
+  ;     :message
+  ;     (map fix-message-val-types!))
+  (let [msgs    (:message (read-messages!))
+        updated (map fix-message-val-types msgs)]
+    updated))
+
+  ;(map (fn [x] (update x :scheduled-date-time #(local-date-time "yyyy/MM/dd HH:mm:ss" %)) (read-messages)))
+  ;(read-messages))
+
+(defn convert-date-time-str
+  "Converts a string to a date-time"
+  [the-date-time-str]
+  (time/local-date-time "yyyy-MM-dd HH:mm:ss" the-date-time-str))
+
+
+
 (defn process-one-message!
   " side effect: post to slack channel "
   [m]
-  (fix-message-val-types! m)
+  (fix-message-val-types m)
   (println m))
   ;(send-to-slack! (:text m)))
 
@@ -55,14 +65,16 @@
   " read all messages from file
     then send each of them to slack "
   []
-  (let [messages (get-messages)
+  (let [messages (get-messages!)
         message (:message messages)]
     (run! process-one-message! message)))
 
 (defn gen-date-time
   " generates a date time"
   []
-  (local-date-time "yyyy/MM/dd HH:mm:ss" "2015/10/01 01:02:03"))
+  (time/local-date-time "yyyy/MM/dd HH:mm:ss" "2015/10/01 01:02:03"))
 
+
+#_ (def m (:message (get-messages!)))
 
 
