e7fd49647b8d3b238e323ea90b5710b8e920b0b3
gallimorej <jeff.gallimore@excella.com>
2019-02-02 21:58:47 -0500

    Add files for web app

--- project.clj
+++ project.clj
@@ -1,17 +1,24 @@
 (defproject proj1 "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :url "http://immense-taiga-11702.herokuapp.com"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [clj-http "3.9.1"]
                  [org.clojure/data.json "0.2.6"]
                  [clojure.java-time "0.3.2"]
-                 [com.novemberain/monger "3.1.0"]]
+                 [com.novemberain/monger "3.1.0"]
+                 [compojure "1.4.0"]
+                 [ring/ring-jetty-adapter "1.4.0"]
+                 [environ "1.0.0"]]
 
+  :min-lein-version "2.0.0"
+  :plugins [[environ/environ.lein "0.3.1"]]
+  :hooks [environ.leiningen.hooks]
   :uberjar-name "proj1.jar"
   :profiles
-  {:dev
+  {:production {:env {:production true}}
+   :dev
    {:plugins [[com.jakemccrary/lein-test-refresh "LATEST"]]}}
 
   :main proj1.slack)
